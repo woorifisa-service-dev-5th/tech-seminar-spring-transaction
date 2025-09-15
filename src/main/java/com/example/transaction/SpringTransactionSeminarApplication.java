de64146950fbb3f94e2f0fdc4b66f8471a552003
Jay Youngjae <jea71882@naver.com>
2025-09-15 17:45:25 +0900

    debug code

--- src/main/java/com/example/transaction/SpringTransactionSeminarApplication.java
+++ src/main/java/com/example/transaction/SpringTransactionSeminarApplication.java
@@ -1,7 +1,10 @@
 package com.example.transaction;
 
+import com.example.transaction.service.DeclarativeTxOrderService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringTransactionSeminarApplication {
@@ -10,4 +13,13 @@ public class SpringTransactionSeminarApplication {
 		SpringApplication.run(SpringTransactionSeminarApplication.class, args);
 	}
 
+    @Bean
+    CommandLineRunner runner(DeclarativeTxOrderService orderService) {
+        return args -> {
+            // 여기에 브레이크포인트도 설정 가능
+            System.out.println("서비스 호출 시작");
+            orderService.placeOrderWithAnno(1L, 5, false);
+            System.out.println("서비스 호출 완료");
+        };
+    }
 }
